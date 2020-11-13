ff7ac94881b77c6665df148fd28b74aa384190a9
Emily Sesto <emilyalicesesto@gmail.com>
2020-11-13 20:40:55 +0000

    Tests passing, display note tested with mocked webpage, add note tested only with createNote method:

--- spec/note-controller-tests.js
+++ spec/note-controller-tests.js
@@ -4,40 +4,28 @@
   document.body.innerHTML = (`
   <form id=text action=#notes/new method=post>
     <input type=text id=text-area>
-    <input id='newnote' type=submit>
+    <input id="submitbutton" type=submit>
   </form>
 `)
-  class DoubleNote {
-    getText(){
-      return ""
-    }
-  }
-  class DoubleNoteList{
-
-  }
- 
   app.setAttribute('id', 'app');
   document.body.appendChild(app);
 
+  var notelist = new NoteList();
+  var noteController = new NoteController(notelist);
+
   function noteControllerCanBeInstantiated() {
-    let notelist = new NoteList();
-    let noteController = new NoteController(notelist);
     assert.isTrue(typeof(noteController) === "object")
     console.log("CAN BE INSTANTIATED")
   }
  
   function canCreateEmptyHtmlList() {
-    let notelist = new NoteList();
-    let noteController = new NoteController(notelist);
     noteController.insertHtml();
     assert.isTrue(app.innerHTML == '<ul></ul>');
     console.log("CAN CREATE EMPTY HTML LIST")
   }
 
   function canAddNoteAndUrl() {
-    let notelist = new NoteList();
     notelist.newNote("I am the first note to be created here")
-    let noteController = new NoteController(notelist);
     noteController.insertHtml();
 
     assert.isTrue(app.innerHTML == '<ul><li><a id="0" href="#notes/0">I am the first note </a></li></ul>');
@@ -45,27 +33,24 @@
   }
 
   function canDisplayNote() {
+    notelist.newNote("I am the first note to be created here")
+
     let note = document.getElementById(0)
+    noteController.listenForHash()
     note.click();
-    setTimeout(function() { assert.isTrue(app.innerHTML == '<div>I am the first note to be created here</div>') }, 7000);
+    setTimeout(function() { assert.isTrue(app.innerHTML == '<div>I am the first note to be created here</div>') }, 2000);
     console.log("CAN DISPLAY NOTE")
   }
 
   function addsNewNotesFromForm(){
-    let notelist = new NoteList();
-    notelist.newNote("test")
-    let noteController = new NoteController(notelist);
-    let input = document.getElementById("text-area")
-    input.value = "New note via form"
-  //document.getElementById("text").submit().reset()
-    noteController.submitForm()
-   // document.getElementById("text").submit()
-   setTimeout(function() { assert.isTrue(app.innerHTML == '<ul><li><a id="0" href="#notes/0">New note via form</a></li></ul>') }, 7000);
+    noteController.createNote("New note via form")
+    console.log(app.innerHTML)
+    assert.isTrue(app.innerHTML == '<ul><li><a id="0" href="#notes/0">I am the first note </a></li><li><a id="1" href="#notes/1">New note via form</a></li></ul>')
   }
 
   noteControllerCanBeInstantiated()
   canCreateEmptyHtmlList()
   canAddNoteAndUrl()
-  canDisplayNote()
   addsNewNotesFromForm()
+  canDisplayNote()
 })(this);
